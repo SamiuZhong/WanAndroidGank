6cbd9a6b3597f9ba9599596721b514f039518857
SamiuZhong <samiuzhong@outlook.com>
2020-03-04 13:46:00 +0800

    bug修改

---
+++ app/src/main/java/com/samiu/host/ui/fragment/wan/WanSearchFragment.kt
@@ -0,0 +1,15 @@
+package com.samiu.host.ui.fragment.wan
+
+import com.samiu.base.ui.BaseFragment
+import com.samiu.host.R
+
+/**
+ * @author Samiu 2020/3/4
+ */
+class WanSearchFragment:BaseFragment() {
+    override fun getLayoutResId()= R.layout.fragment_wan_search
+
+    override fun initView() =Unit
+
+    override fun initData()=Unit
+}
