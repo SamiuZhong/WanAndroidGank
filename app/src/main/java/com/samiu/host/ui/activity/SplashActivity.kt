13b87d5b6d1aa169cc277c4aa9b76bb32de48705
SamiuZhong <samiuzhong@outlook.com>
2020-03-20 13:37:07 +0800

    启动页优化

--- app/src/main/java/com/samiu/host/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/samiu/host/ui/activity/SplashActivity.kt
@@ -1,25 +1,14 @@
 package com.samiu.host.ui.activity
 
 import android.content.Intent
-import com.samiu.base.ui.BaseActivity
-import com.samiu.host.R
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-/**
- * @author Samiu 2020/3/20
- */
-class SplashActivity :BaseActivity() {
-    override fun getLayoutResId() = R.layout.activity_splash
+class SplashActivity : AppCompatActivity() {
 
-    override fun initView() = Unit
-
-    override fun initData() {
-        startActivity(Intent(this,MainActivity::class.java))
-    }
-
-    suspend fun start(){
-        MainScope().launch {
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
