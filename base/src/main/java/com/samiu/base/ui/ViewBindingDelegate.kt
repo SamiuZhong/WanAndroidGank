a98a1e199bd185dcc4453d9ef5a9df6f52aec877
SamiuZhong <samiuzhong@outlook.com>
2020-03-27 15:25:47 +0800

    ViewBinding委托优化

--- base/src/main/java/com/samiu/base/ui/ViewBindingDelegate.kt
+++ base/src/main/java/com/samiu/base/ui/ViewBindingDelegate.kt
@@ -1,6 +1,8 @@
 package com.samiu.base.ui
 
+import android.view.LayoutInflater
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
@@ -9,6 +11,24 @@ import androidx.lifecycle.observe
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
+
+/**
+ * @author Samiu 2020/3/27
+ */
+
+/**
+ * Activity ViewBinding Delegate
+ */
+inline fun <T : ViewBinding> AppCompatActivity.viewBinding(crossinline bindingInflater: (LayoutInflater) -> T) =
+    lazy(LazyThreadSafetyMode.NONE) {
+        bindingInflater.invoke(layoutInflater)
+    }
+
+/**
+ * Fragment ViewBinding Delegate
+ */
+fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
+    FragmentViewBindingDelegate(this, viewBindingFactory)
 
 class FragmentViewBindingDelegate<T : ViewBinding>(
     val fragment: Fragment,
@@ -44,6 +64,3 @@ class FragmentViewBindingDelegate<T : ViewBinding>(
         return viewBindingFactory(thisRef.requireView()).also { _binding = it }
     }
 }
-
-fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
-    FragmentViewBindingDelegate(this, viewBindingFactory)
